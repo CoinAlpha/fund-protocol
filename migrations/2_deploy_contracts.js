57cc27e1f22a368121aef16de3e995ce8628c108
Michael Feng <mifeng@gmail.com>
2017-09-28 21:34:20 -0700

    use fund defaults

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -4,19 +4,19 @@ var InvestorActions = artifacts.require("./InvestorActions.sol");
 var Fund = artifacts.require("./Fund.sol");
 
 // Deployment constants
-const managerInvestment = 1e17;
+const managerInvestment = 0;
 
 module.exports = function(deployer, network, accounts) {
 
   const useOraclize = network == "ropsten" ? true : false;
-  const dataFeedReserve = network == "ropsten" ? 1e17 : 0;
+  const dataFeedReserve = network == "ropsten" ? 1e18 : 0;
   
   deployer.deploy(
     DataFeed,
     "nav-service",                    // _name
     useOraclize,                      // _useOraclize
     "json(http://9afaae62.ngrok.io/api/sandbox).totalPortfolioValueEth", // _queryUrl
-    60,                               // _secondsBetweenQueries
+    300,                              // _secondsBetweenQueries
     accounts[1],                      // _exchange
     {from: accounts[0], value: dataFeedReserve}
   ).then(() =>
@@ -35,11 +35,11 @@ module.exports = function(deployer, network, accounts) {
       "Falcon",                       // _name
       "FALC",                         // _symbol
       4,                              // _decimals
-      4e17,                           // _minInitialSubscriptionEth
-      1e17,                           // _minSubscriptionEth
-      1e17,                           // _minRedemptionShares,
+      20e18,                          // _minInitialSubscriptionEth
+      5e18,                           // _minSubscriptionEth
+      5e18,                           // _minRedemptionShares,
       100,                            // _mgmtFeeBps
-      2000,                           // _performFeeBps
+      0,                              // _performFeeBps
       {from: accounts[0], value: managerInvestment}
   ));
 };
