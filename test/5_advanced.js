57749338866500f1edea0bb1036fa2fc0d2fed37
kluwena <karelluwena@gmail.com>
2017-10-10 19:11:20 -0700

    test #5 eslint fix

--- test/5_advanced.js
+++ test/5_advanced.js
@@ -23,16 +23,9 @@ const diffInWei = (a, b) => weiToNum(a) - weiToNum(b);
 const gasToWei = gas => gas * 1e11;
 
 contract('Advanced', (accounts) => {
-<<<<<<< HEAD
-
-  const MANAGER = accounts[0];
-  const EXCHANGE = accounts[1];
-
   // test parameters
-=======
   const MANAGER = accounts[0];
   const EXCHANGE = accounts[1];
->>>>>>> fixes to test #5
   const GAS_AMT = 500000;
   const USD_ETH = 300;
   const MIN_INITIAL_SUBSCRIPTION = 5;
@@ -41,12 +34,9 @@ contract('Advanced', (accounts) => {
   const MGMT_FEE = 1;
   const PERFORM_FEE = 20;
   const SECONDS_IN_YEAR = 31536000;
-<<<<<<< HEAD
-  
-  const investors = accounts.slice(2);
-=======
   const PERFORM_FEE_BPS = 2000;
 
+  // contract instances
   let fund;
   let navCalculator;
   let valueFeed;
@@ -55,10 +45,7 @@ contract('Advanced', (accounts) => {
   const getBal = address => web3.fromWei(web3.eth.getBalance(address), 'ether').toNumber();
   const weiToNum = wei => web3.fromWei(wei, 'ether').toNumber();
   const ethToWei = eth => web3.toWei(eth, 'ether');
->>>>>>> fixes to test #5
 
-  // contract instances
-  let dataFeed, fund, navCalculator, investorActions;
 
   before(() => DataFeed.new(
     'nav-service',                          // _name
@@ -67,13 +54,13 @@ contract('Advanced', (accounts) => {
     300,                                    // _secondsBetweenQueries
     USD_ETH * 100,                          // _initialExchangeRate
     EXCHANGE,                               // _exchange
-    { from: MANAGER, value: 0 }
+    { from: MANAGER, value: 0 },
   )
-    .then(instance => {
+    .then((instance) => {
       dataFeed = instance;
       return Promise.all([
         NavCalculator.new(dataFeed.address, { from: MANAGER }),
-        InvestorActions.new(dataFeed.address, { from: MANAGER })
+        InvestorActions.new(dataFeed.address, { from: MANAGER }),
       ]);
     })
     .then((contractInstances) => {
@@ -83,52 +70,45 @@ contract('Advanced', (accounts) => {
         navCalculator.address,              // _navCalculator
         investorActions.address,            // investorActions
         dataFeed.address,                   // _dataFeed
-        "TestFund",                         // _name
-        "TEST",                             // _symbol
+        'TestFund',                         // _name
+        'TEST',                             // _symbol
         4,                                  // _decimals
         ethToWei(MIN_INITIAL_SUBSCRIPTION), // _minInitialSubscriptionEth
         ethToWei(MIN_SUBSCRIPTION),         // _minSubscriptionEth
         MIN_REDEMPTION_SHARES,              // _minRedemptionShares,
         MGMT_FEE * 100,                     // _mgmtFeeBps
         PERFORM_FEE * 100,                  // _performFeeBps
-        { from: MANAGER }
+        { from: MANAGER },
       );
     })
     .then((fundInstance) => {
       fund = fundInstance;
       return Promise.all([
         navCalculator.setFund(fund.address),
-        investorActions.setFund(fund.address)
+        investorActions.setFund(fund.address),
       ]);
     })
     .then(() => Promise.all([
       navCalculator.fundAddress.call({ from: MANAGER }),
       investorActions.fundAddress.call({ from: MANAGER }),
-      dataFeed.updateWithExchange(100)
+      dataFeed.updateWithExchange(100),
     ]))
-    .then(() => {
-      return Promise.all(investors.map(acct => fund.modifyAllocation(acct, ethToWei(30))));
-    })
+    .then(() => Promise.all(investors.map(acct => fund.modifyAllocation(acct, ethToWei(30)))))
     .catch(err => console.log('**** BEFORE ERROR: ', err)));
 
   beforeEach((done) => {
     console.log('**** Resetting subscription ****');
     Promise.all(investors.map(acct => fund.requestSubscription({ from: acct, value: ethToWei(MIN_INITIAL_SUBSCRIPTION) })))
-      .then(() => {
-        // Gas for subscribing a single investor ~=81800
-        return Promise.all(investors.map(acct => fund.getInvestor(acct)));
-      })
+      .then(() => Promise.all(investors.map(acct => fund.getInvestor(acct))))     // Gas for subscribing a single investor ~=81800
       .then((_values) => {
-        _values.forEach((val, i) => {
+        _values.forEach((val) => {
           assert(weiToNum(val[1]) !== 0, 'Subscription Request failed');
         });
         return fund.fillAllSubscriptionRequests();
       })
-      .then(() => {
-        return Promise.all(investors.map(acct => fund.getInvestor(acct)));
-      })
+      .then(() => Promise.all(investors.map(acct => fund.getInvestor(acct))))
       .then((_values) => {
-        _values.forEach((val, i) => {
+        _values.forEach((val) => {
           assert.equal(weiToNum(val[1]), 0, 'Subscription failed: incorrect ethPendingSubscription');
           assert(weiToNum(val[2]) !== 0, 'Subscription failed: incorrect balance');
         });
@@ -140,13 +120,9 @@ contract('Advanced', (accounts) => {
   // Gas for redeeming a single investor ~=399000
   it('should redeem all redemption requests', (done) => {
     fund.remitFromExchange({ from: EXCHANGE, value: ethToWei(99), gas: GAS_AMT })
-      .then(() => {
-        return Promise.all(investors.map(acct => fund.requestRedemption(MIN_REDEMPTION_SHARES, { from: acct })));
-      })
+      .then(() => Promise.all(investors.map(acct => fund.requestRedemption(MIN_REDEMPTION_SHARES, { from: acct }))))
       .then(() => fund.fillAllRedemptionRequests())
-      .then(() => {
-        return Promise.all(investors.map(acct => fund.getInvestor(acct)));
-      })
+      .then(() => Promise.all(investors.map(acct => fund.getInvestor(acct))))
       .then((_values) => {
         _values.forEach((val, index) => {
           assert.equal(+val[3], 0, `redemption index: ${index}, addr: ${val} failed to process`);
@@ -160,9 +136,7 @@ contract('Advanced', (accounts) => {
   // Gas for liquidating a single investor ~=414700
   it('should liquidate all investors', (done) => {
     fund.liquidateAllInvestors()
-      .then(() => {
-        return Promise.all(investors.map(acct => fund.getInvestor(acct)));
-      })
+      .then(() => Promise.all(investors.map(acct => fund.getInvestor(acct))))
       .then((_values) => {
         _values.forEach((val, index) => {
           assert.equal(weiToNum(val[2]), 0, `liquidation index: ${index}, addr: ${val} failed to process`);
@@ -181,11 +155,9 @@ contract('Advanced', (accounts) => {
         fund.fillAllRedemptionRequests();
         return fund.sendToExchange(ethToWei(80));
       })
-      .then(() => {
-        return Promise.all(investors.map(acct => fund.getInvestor(acct)));
-      })
+      .then(() => Promise.all(investors.map(acct => fund.getInvestor(acct))))
       .then((_values) => {
-        _values.forEach((val, index) => {
+        _values.forEach((val) => {
           assert.equal(+val[3], 0, 'Redemption failed due to sendToExchange');
           assert(weiToNum(val[4]) > 0, 'Redemption failed due to sendToExchange');
         });
@@ -199,16 +171,16 @@ contract('Advanced', (accounts) => {
   xit('should not let exchange balance change affect liquidateAll', (done) => {
     fund.liquidateAllInvestors();
     fund.sendToExchange(ethToWei(60))
-      .then(() => {
-        return Promise.all(investors.map(acct => fund.getInvestor(acct)));
-      })
+      .then(() => Promise.all(investors.map(acct => fund.getInvestor(acct))))
       .then((_values) => {
-        _values.forEach((val, index) => {
+        _values.forEach((val) => {
           assert.equal(weiToNum(val[2]), 0, 'Liquidation failed due to sendToExchange');
           assert(weiToNum(val[4]) > 0, 'Liquidation failed due to sendToExchange');
         });
       })
-      .then(() => { done(); })
+      .then(() => {
+        done();
+      })
       .catch(done);
   });
 });
