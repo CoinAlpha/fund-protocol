d1f529f432a7b45abccae26be1581b03fdb48a2a
Carlo P. Las Marias <carlol@gmail.com>
2017-11-13 19:33:27 -0800

    added test object to config

--- migrations/config/datafeed-template.js
+++ migrations/config/datafeed-template.js
@@ -1,6 +1,10 @@
 // Create a file called datafeed.js duplicating this file and replace the values below
 
 module.exports = {
+  test: {
+    navServiceUrl: '[STAGING URL]',
+    dataFeedAddress: '',
+  },
   ropsten: {
     navServiceUrl: '[STAGING URL]',
     dataFeedAddress: '',
