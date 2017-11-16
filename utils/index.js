07d50db361709d796f3842e9f25ebbaf018196c4
Carlo P. Las Marias <carlol@gmail.com>
2017-11-16 00:27:35 -0800

    transferExactAmount net of gas: navCalc test

---
+++ utils/index.js
@@ -0,0 +1,11 @@
+const transferExactAmountPromise = (from, to, _eth) => {
+  const tx = {
+    from,
+    to,
+    value: web3.toWei(_eth, 'ether'),
+  };
+
+  const gasEstimate = web3.eth.estimateGas(tx);
+  const newTx = Object.assign({}, tx, { value: tx.value - gasEstimate });
+  return web3.eth.sendTransactionPromise(newTx);
+};
